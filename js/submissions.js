9c4d5fb83f321c79546e24e482d3885670fe2224
Maxosman1 <120883114+Maxosman1@users.noreply.github.com>
2023-11-18 18:19:20 -0600

    1

--- js/submissions.js
+++ js/submissions.js
@@ -1,26 +1,3 @@
-import { initializeApp } from "firebase/app";
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  updateDoc,
-  doc
-} from "firebase/firestore";
-
-// Firebase configuration (replace with your config)
-const firebaseConfig = {
-  apiKey: "AIzaSyBD5NQbPThN7wO0vID2Uvg1fb_JCCS1l04",
-  authDomain: "styrate-2ca66.firebaseapp.com",
-  projectId: "styrate-2ca66",
-  storageBucket: "styrate-2ca66.appspot.com",
-  messagingSenderId: "56860576310",
-  appId: "1:56860576310:web:0270c6c470c05a16602697",
-  measurementId: "G-YH58NS4ZHT"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
 // Function to display videos
 async function displayVideos() {
